c1a37ad37ed78f421be840d74c7110a1d73e537a
Praveer Rai <raipraveer@gmail.com>
2016-01-08 02:15:44 +0100

    added winning conditions, game almost done

--- app/src/main/java/com/praveer_rai/connect3/MainActivity.java
+++ app/src/main/java/com/praveer_rai/connect3/MainActivity.java
@@ -8,23 +8,86 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.animation.Animation;
+import android.widget.GridLayout;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    int activePlayer = 0; // yellow
+    int activePlayer = 1; // yellow, 2-> red
 
-    public void dropIn(View view){
-        ImageView counter = (ImageView)view;
-        counter.setTranslationY(-1000f);
-        if(activePlayer == 0) {
-            counter.setImageResource(R.drawable.yellow);
-            activePlayer = 1;
-        } else {
-            counter.setImageResource(R.drawable.red);
-            activePlayer = 0;
+    int[] gameState = new int[10]; // for readability 10 instead of 9
+
+    int[][] winningPositions = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}, {1, 4, 7}, {2, 5, 7}, {3, 6, 9}, {1, 5, 9}, {3, 5, 7},};
+
+    TextView winMsg = null;
+    LinearLayout playAgain = null;
+    GridLayout gridLayout = null;
+
+    boolean isGameActive = true;
+
+    public void dropIn(View view) {
+
+        ImageView currentBox = (ImageView) view;
+
+        int currentBoxInt = Integer.parseInt(currentBox.getTag().toString());
+
+        if (isGameActive && gameState[currentBoxInt] == 0) {
+
+            gameState[currentBoxInt] = activePlayer;
+
+            currentBox.setTranslationY(-1000f);
+
+            if (activePlayer == 1) {
+                currentBox.setImageResource(R.drawable.yellow);
+                activePlayer = 2;
+            } else {
+                currentBox.setImageResource(R.drawable.red);
+                activePlayer = 1;
+            }
+
+            currentBox.animate().translationYBy(1000f).rotation(720f).setDuration(600);
         }
-        counter.animate().translationYBy(1000f).rotation(720f).setDuration(600);
+
+        checkIfWin();
+
+    }
+
+    public void checkIfWin() {
+        for (int[] wp : winningPositions) {
+            // check if all positions are played
+            if (gameState[wp[0]] != 0 && // no need to check all 3, it would be false anyway in next condition
+                    // if same player state at all of these winning positions
+                    gameState[wp[0]] == gameState[wp[1]] &&
+                    gameState[wp[1]] == gameState[wp[2]]) {
+
+                // win condition
+
+                int winner = gameState[wp[0]];
+                String winPlayer = "Yellow";
+                if (winner == 2) {
+                    winPlayer = "Red";
+                }
+
+                winMsg.setText(winPlayer + " Wins!");
+                playAgain.setVisibility(View.VISIBLE);
+                isGameActive = false;
+
+            }
+        }
+    }
+
+    public void playAgain(View view) {
+        playAgain.setVisibility(View.INVISIBLE);
+        gameState = new int[10]; // for readability 10 instead of 9
+
+        for (int i = 0; i < gridLayout.getChildCount(); i++) {
+            ImageView img = (ImageView) gridLayout.getChildAt(i);
+            img.setImageResource(0); // for no image
+        }
+        isGameActive = true;
     }
 
     @Override
@@ -33,6 +96,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        winMsg = (TextView) findViewById(R.id.winnerMessage);
+        playAgain = (LinearLayout) findViewById(R.id.playAgainLayout);
+        gridLayout = (GridLayout) findViewById(R.id.gridLayout);
     }
 
     @Override
